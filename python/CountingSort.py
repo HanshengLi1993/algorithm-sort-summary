56ea885cef80f196bd84cc707cb3b042a5bae7ef
HanshengLi1993 <37957402+HanshengLi1993@users.noreply.github.com>
2018-10-19 08:55:46 +0800

    Add files via upload
    add common out in CountingSort and RadixSort

--- python/CountingSort.py
+++ python/CountingSort.py
@@ -2,15 +2,19 @@
 
 
 def counting_sort(clist):
+    # 构造桶
     bucketLength = max(clist) + 1
     bucket = [0] * bucketLength
+
     sortedIndex = 0
     length = len(clist)
+    # 将元素装入桶中，对应桶的数量加一表示该桶中相同数据的数量
     for i in range(length):
         if not bucket[clist[i]]:
             bucket[clist[i]] = 0
         bucket[clist[i]] += 1
 
+    # 将桶中元素去除，并填充到有序数组中
     for j in range(bucketLength):
         while bucket[j] > 0:
             clist[sortedIndex] = j
--- python/RadixSort.py
+++ python/RadixSort.py
@@ -2,24 +2,34 @@
 
 
 def radix_sort(rlist, radix=10):
+    """
+
+    :param rlist:待排序数组
+    :param radix: 基数
+    :return:
+    """
     length = len(rlist)
     if rlist is None or length < 2:
         return rlist
     maxValue = max(rlist)
     maxDigit = 0
+    # 获取数组最大值的位数
     while maxValue != 0:
         maxValue //= 10  # python3里的"/"表示 浮点数除法,返回浮点结果,"//"表示整数除法
         maxDigit += 1
     mod = radix
     div = 1
+    # 初始化桶
     bucketList = []
     for i in range(mod):
         bucketList.append([])
     for i in range(maxDigit):
+        # 将数据按从低到高位，从各位开始逐层向上排序，并按排序的位将数据装入桶中得到此位数据有序
         for j in range(length):
             num = int((rlist[j] % mod) / div)
             bucketList[num].append(rlist[j])
         index = 0
+        # 将基数桶中的数据取出，得到部分有序的数组
         for j in range(len(bucketList)):
             for k in range(len(bucketList[j])):
                 rlist[index] = bucketList[j][k]
